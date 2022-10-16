6fbe5bedf72fc91c108dcfd3cc31d8827be281ed
Paulo <paulougarte29@gmail.com>
2022-10-16 01:33:54 -0500

    añadiedo mejoras

--- js/main.js
+++ js/main.js
@@ -16,3 +16,27 @@ function ocultar_menu() {
     menuback.style.display = 'none';
 }
 
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //po defecto está en horizontal
+    //direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
