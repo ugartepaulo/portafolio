9002a85e1b4c7dea6e18998661814a161a044bf5
Paulo <paulougarte29@gmail.com>
2022-10-17 17:45:25 -0500

    cambiando estilos del slider

--- js/main.js
+++ js/main.js
@@ -20,10 +20,13 @@ function ocultar_menu() {
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    //po defecto está en horizontal
+    //por defecto está en horizontal
     //direction: 'vertical',
     loop: true,
-
+    //velocidad en la que cambia cada slider
+    speed:700,
+    effect: 'slide',
+    
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
@@ -39,4 +42,33 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
         el: '.swiper-scrollbar',
     },
+
+    autoplay:{
+        delay: 4000
+    },
+
+    slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    1200:{
+        slidesPerView: 4,
+        spaceBetween: 40
+    }
+  }
 });
